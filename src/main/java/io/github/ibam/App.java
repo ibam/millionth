371d5ae9b0a4ce3a860cc9680106f5d0757ce3cc
Ibrahim Arief <ibam.arief@gmail.com>
2015-12-17 02:07:51 +0100

    Initial archetype-generated project

---
+++ src/main/java/io/github/ibam/App.java
@@ -0,0 +1,11 @@
+package io.github.ibam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( final String[] args ) {
+        System.out.println( "Hello World! Here are the system properties: " + System.getProperties() );
+    }
+}
---
+++ src/test/java/io/github/ibam/AppTest.java
@@ -0,0 +1,15 @@
+package io.github.ibam;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void test() {
+        System.out.println("Hi!");
+    }
+
+}
